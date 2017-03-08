c06bdee8b955c29878286cd81ea4ff52726a931a
Johannes <jkirschnick@gmail.com>
2017-03-08 14:26:27 +0100

    Update versions

--- src/main/java/com/github/iesen/rabbitmq/plugin/RabbitMQConstants.java
+++ src/main/java/com/github/iesen/rabbitmq/plugin/RabbitMQConstants.java
@@ -9,12 +9,12 @@ import java.io.File;
 public class RabbitMQConstants {
 
     public static final String RABBITMQ_DEFAULT_PORT = "5672";
-    public static final String RABBITMQ_VERSION = "3.4.3";
+    public static final String RABBITMQ_VERSION = "3.6.6";
     public static final String RABBITMQ_PARENT_DIR = SystemUtils.getUserHome() + File.separator + ".rabbitmq_maven_plugin";
     public static final String RABBITMQ_HOME = RABBITMQ_PARENT_DIR + File.separator + "rabbitmq_server-" + RABBITMQ_VERSION;
-    public static final String ERLANG_HOME_WIN = System.getenv("ProgramFiles(X86)") + File.separator + "erl6.3";
-    public static final String ERLANG_INSTALLER_URL = "http://www.erlang.org/download/otp_win32_17.4.exe";
-    public static final String ERLANG_INSTALLER_FILE_NAME = "otp_win32_17.4.exe";
+    public static final String ERLANG_HOME_WIN = System.getenv("ProgramFiles") + File.separator + "erl8.2";
+    public static final String ERLANG_INSTALLER_URL = "http://erlang.org/download/otp_win64_19.2.exe";
+    public static final String ERLANG_INSTALLER_FILE_NAME = "otp_win64_19.2.exe";
 
 
     private RabbitMQConstants() {
